8b623df10e2e4ef78ecadfd4a2cb06e223cfdb9f
muteeabdullah <muteeabdullah08@gmail.com>
2023-05-21 14:43:17 +0500

    android

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_flutter/adapters.dart';
+
 import 'pages/home_page.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   // init the hive
   await Hive.initFlutter();
 
-  // open a box
-  var box = await Hive.openBox('mybox');
+  // open a box 
 
   runApp(const MyApp());
 }
